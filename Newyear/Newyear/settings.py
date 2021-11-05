70e055fe7ec3b30a820dc3f48f82897527ff3c59
venkykatta <venkateshkatta9705@gmail.om>
2021-11-05 19:43:32 +0530

    adding static path

--- Newyear/Newyear/settings.py
+++ Newyear/Newyear/settings.py
@@ -124,4 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
